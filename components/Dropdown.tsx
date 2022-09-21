2ab753e8fe0255996dee8a4df9b94069fbec3ef7
LK152 <wuluke0@gmail.com>
2022-09-21 15:23:21 +0800

    l

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,7 +1,26 @@
-const Dropdown = (props: dropdownProps) => {
-    return (
-        <div></div>
-    )
-}
+import { Button, Typography } from '@mui/material';
+import styles from '@styles/Dropdown.module.css';
+import NextMuiLink from './NextMuiLink';
 
-export default Dropdown
+const Dropdown = (props: dropdownProps) => {
+	return (
+		<div className={styles.dropdown}>
+			<Button>
+				<Typography>{props.title}</Typography>
+			</Button>
+			<ul className={styles.content}>
+				{props.subItems.map(({ title, path }, idx) => {
+					return (
+						<li key={idx}>
+							<a>
+								<Typography>{title}</Typography>
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
+
+export default Dropdown;
