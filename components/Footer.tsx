4e3043b1332f2d8a8b89714c8ce45018220aab1d
LK152 <wuluke0@gmail.com>
2022-09-27 14:48:20 +0800

    add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,40 @@
+import usePreloadCtx from '@context/PreloadCtx';
+import { Box, Stack, Typography } from '@mui/material';
+import NextMuiLink from './NextMuiLink';
+
 const Footer = () => {
-    return (
-        <div></div>
-    )
-}
+	const { navbarCtx } = usePreloadCtx();
+
+	return (
+		<Box
+			component='footer'
+			sx={{
+				width: '100%',
+				height: '480px',
+				display: 'flex',
+				alignItems: 'center', 
+                justifyContent: 'center', 
+                backgroundColor: '#192148'
+			}}
+		>
+			<Stack direction='row' spacing={4}>
+				{navbarCtx.map(({ title, subItems }, idx) => {
+					return (
+						<Stack key={idx} direction='column'>
+							<Typography fontWeight='bold' fontSize='1.5rem' color='white'>{title}</Typography>
+							{subItems.map(({ title, path }, i) => {
+								return (
+									<NextMuiLink key={i} href={path} sx={{textDecoration: 'none'}}>
+										<Typography color='white'>{title}</Typography>
+									</NextMuiLink>
+								);
+							})}
+						</Stack>
+					);
+				})}
+			</Stack>
+		</Box>
+	);
+};
 
 export default Footer;
---
+++ pages/math.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const math: NextPage = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default math;
