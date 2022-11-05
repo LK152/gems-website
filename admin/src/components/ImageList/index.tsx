07eb679964064117fe86e4f4692233c9f6a975e9
LK152 <wuluke0@gmail.com>
2022-11-05 23:09:05 +0800

    sw

--- admin/src/components/ImageList/index.tsx
+++ admin/src/components/ImageList/index.tsx
@@ -1,19 +1,48 @@
-import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
-import { Box, Card, Paper, Stack } from '@mui/material';
+import { Delete, KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
+import {
+	Box,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	IconButton,
+	Paper,
+	Stack,
+	Typography,
+} from '@mui/material';
 
 const ImageList = ({ images }: { images: imageProps[] | null }) => {
 	console.log(images);
 
 	return (
 		<Paper variant='outlined' sx={{ width: '80%', mx: 'auto' }}>
-			<Stack direction='row' overflow='hidden'>
-				{images?.map(({ path }, idx) => (
-					<Card sx={{minWidth: '100px'}} key={idx}>
-						<img
+			<Stack direction='row' alignItems='center' overflow='scroll'>
+				{images?.map(({ path, order }, idx) => (
+					<Card
+						sx={{ minWidth: '300px', height: 'fit-content', mx: 4 }}
+						key={idx}
+					>
+						<CardMedia
+							component='img'
+							image={`http://localhost:8000/${path}`}
 							alt='slider'
-							width='100%'
-							src={`http://localhost:8000/${path}`}
 						/>
+						<CardContent>
+							<Typography>{`Order: ${order + 1}`}</Typography>
+						</CardContent>
+						<CardActions>
+							<Stack width='100%' direction='row' justifyContent='space-between'>
+								<IconButton>
+									<KeyboardArrowLeft />
+								</IconButton>
+                                <IconButton>
+                                    <Delete />
+                                </IconButton>
+								<IconButton>
+									<KeyboardArrowRight />
+								</IconButton>
+							</Stack>
+						</CardActions>
 					</Card>
 				))}
 			</Stack>
